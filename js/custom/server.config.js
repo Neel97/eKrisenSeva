236348c3ea9704a62acdb781ff06cd546abbf293
Maulik212 <maulikshah21297@gmail.com>
2018-03-29 21:37:59 +0530

    Server.config path changed

--- js/custom/server.config.js
+++ js/custom/server.config.js
@@ -1,4 +1,4 @@
 var isDemo = true;
-var serverName = "http://" + ((isDemo)? "localhost/eKrisenSeva/www/": "an0nymox.ml/");
+var serverName = "http://" + ((isDemo)? "localhost/eKrisenSeva/": "an0nymox.ml/");
 // serverName = "";
 // console.log(serverName);
